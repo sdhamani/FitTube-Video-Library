fded59fe9a0bf81c05193e66778fc1830cf3af61
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-10 05:20:48 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-
+import LandingPage from "./components/LandingPage";
+import PlayVideo from "./components/PlayVideo";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/playvideo" element={<PlayVideo />} />
+      </Routes>
     </div>
   );
 }
