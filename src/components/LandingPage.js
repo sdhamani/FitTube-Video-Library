fb37330db736af2aa0ac27ea311586f8eb4b30b5
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-13 04:36:07 +0530

    Mapped the videos to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,30 +2,32 @@ import React from "react";
 import SideBar from "./SideBar";
 
 import { Link } from "react-router-dom";
+import videos from "../data/videos";
 
 function LandingPage() {
   return (
     <div className="landing-page-main">
       <SideBar />
       <div className="landing-page-div">
-        <Link className="landing-page-videos" to="/playvideo">
-          <div>
-            <div className="video">
-              <img
-                className="video-image"
-                src="https://i.ytimg.com/an_webp/ml6cT4AZdqI/mqdefault_6s.webp?du=3000&sqp=CLiGw4MG&rs=AOn4CLB-LaZUNk58eM1NuzaqAD6cM8Yqsw"
-                alt="NA"
-              />
-              <div className="video-content">
-                <div className="video-name">
-                  30-Minute HIIT Cardio Workout with Warm Up
-                </div>
-                <div className="video-channel">SELF</div>
-                <div className="video-channel">31M views 2 years ago</div>
+        <div classname="videos">
+          {videos.map((item) => {
+            return (
+              <div className="video">
+                <Link
+                  className="landing-page-videos"
+                  to={`/playvideo/${item.id}`}
+                >
+                  <img className="video-image" src={item.image} alt="NA" />
+                  <div className="video-content">
+                    <div className="video-name">{item.name}</div>
+                    <div className="video-channel">{item.channel}</div>
+                    <div className="video-channel">31M views 2 years ago</div>
+                  </div>
+                </Link>
               </div>
-            </div>
-          </div>
-        </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
