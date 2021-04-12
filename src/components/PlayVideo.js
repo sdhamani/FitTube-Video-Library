780c3b3de75e88ceaa91b5b8c5ad46e12167e2a4
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-13 04:36:30 +0530

    Used params to play videos

--- src/components/PlayVideo.js
+++ src/components/PlayVideo.js
@@ -1,48 +1,54 @@
 import React from "react";
 import SideBar from "./SideBar";
+import videos from "../data/videos";
+import { useParams } from "react-router-dom";
 
 function PlayVideo() {
+  const { id } = useParams();
+  console.log(id);
+  const video = videos.find((item) => item.id === id);
+
   return (
     <div className="playvideo-div">
       <SideBar />
-      <div className="playvideo">
-        <iframe
-          width="1020"
-          height="500"
-          src="https://www.youtube.com/embed/XVxhE3MIjFU"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen="1"
-        ></iframe>
-        <div className="video-content">
-          <div className="video-name">
-            30-Minute HIIT Cardio Workout with Warm Up
-          </div>
-          <div className="video-play-actions">
-            <div className="video-play-views">
-              <div className="video-channel">SELF</div>
-              <div className="video-channel">31M views 2 years ago</div>
-            </div>
-            <div>
-              <i class="fa fa-thumbs-o-up fa-lg" aria-hidden="true"></i>
-            </div>
-            <div>
-              <i class="fa fa-thumbs-o-down fa-lg" aria-hidden="true"></i>
-            </div>
-            <div>
-              <i class="fa fa-clock-o fa-lg" aria-hidden="true"></i>
-              Watch later
-            </div>
-            <div>
-              <i class="fa fa-play-circle fa-lg" aria-hidden="true">
-                {" "}
-              </i>
-              Add to Playlist
+      {video && (
+        <div className="playvideo">
+          <iframe
+            width="1020"
+            height="500"
+            src={video.videoLink}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <div className="video-content">
+            <div className="video-name">{video.name}</div>
+            <div className="video-play-actions">
+              <div className="video-play-views">
+                <div className="video-channel">{video.channel}</div>
+                <div className="video-channel">31M views 2 years ago</div>
+              </div>
+              <div>
+                <i class="fa fa-thumbs-o-up fa-lg" aria-hidden="true"></i>
+              </div>
+              <div>
+                <i class="fa fa-thumbs-o-down fa-lg" aria-hidden="true"></i>
+              </div>
+              <div>
+                <i class="fa fa-clock-o fa-lg" aria-hidden="true"></i>
+                Watch later
+              </div>
+              <div>
+                <i class="fa fa-play-circle fa-lg" aria-hidden="true">
+                  {" "}
+                </i>
+                Add to Playlist
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
