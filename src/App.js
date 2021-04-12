87c270024192897ce2b98360e87d89e1900f859e
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-13 04:36:51 +0530

    Included id in the route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import NavBar from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
 import PlayVideo from "./components/PlayVideo";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/playvideo" element={<PlayVideo />} />
+        <Route path="/playvideo/:id" element={<PlayVideo />} />
       </Routes>
     </div>
   );
