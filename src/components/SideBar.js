31054ee5cd933e8a1ec279fba76aaaebd0d089c9
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-18 04:27:26 +0530

    Updated styling

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,28 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function SideBar() {
   return (
     <div className="action-bar">
       <div className="actions-div">
         <div className="actions-types">
-          <i class="fa fa-home fa-2x badge-icons" aria-hidden="true"></i>{" "}
-          <label className="sidebar-label">Home</label>
+          <Link className="link-no-decoration" to="/">
+            <i class="fa fa-home fa-2x badge-icons" aria-hidden="true"></i>{" "}
+            <label className="sidebar-label">Home</label>
+          </Link>
         </div>
         <div className="actions-types">
-          <i class="fa fa-history fa-2x badge-icons" aria-hidden="true"></i>{" "}
-          <label className="sidebar-label">History</label>
+          <Link className="link-no-decoration" to="/history">
+            <i class="fa fa-history fa-2x badge-icons" aria-hidden="true"></i>{" "}
+            <label className="sidebar-label">History</label>
+          </Link>
         </div>
         <div className="actions-types">
-          <i class="fa fa-play-circle fa-2x badge-icons" aria-hidden="true"></i>{" "}
-          <label className="sidebar-label">Playlist</label>
+          <Link className="link-no-decoration" to="/">
+            <i
+              class="fa fa-play-circle fa-2x badge-icons"
+              aria-hidden="true"
+            ></i>{" "}
+            <label className="sidebar-label">Playlist</label>
+          </Link>
         </div>
         <div className="actions-types">
-          <i class="fa fa-clock-o fa-2x badge-icons" aria-hidden="true"></i>
-          <label className="sidebar-label">Watch Later</label>
+          <Link className="link-no-decoration" to="/watchlater">
+            <i class="fa fa-clock-o fa-2x badge-icons" aria-hidden="true"></i>
+            <label className="sidebar-label">Watch Later</label>
+          </Link>
         </div>
         <div className="actions-types">
-          <i class="fa fa-thumbs-up fa-2x badge-icons" aria-hidden="true"></i>
-          <label className="sidebar-label">Liked Videos</label>
+          <Link className="link-no-decoration" to="/likedvideos">
+            <i class="fa fa-thumbs-up fa-2x badge-icons" aria-hidden="true"></i>
+            <label className="sidebar-label">Liked Videos</label>
+          </Link>
         </div>
       </div>
     </div>
