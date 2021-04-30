7dbc92cc6ea3ad4a41605094331c97ce546edc1c
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-01 03:26:09 +0530

    Added Footer comonent

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Routes, Route, NavLink, Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <ul className="list-no-bullets nav-pills nav-list-ecom">
+        <li className="list-item-inline">
+          <NavLink
+            to="/"
+            className="list-item-inline-link"
+            activeClassName="nav-active-icon"
+          >
+            {" "}
+            <div className="badge-div">
+              <i class="fa fa-home fa-lg badge-icons" aria-hidden="true"></i>{" "}
+            </div>
+          </NavLink>
+        </li>
+        <li className="list-item-inline">
+          <NavLink to="/history" activeClassName="nav-active-icon">
+            <div className="badge-div">
+              <i class="fa fa-history fa-lg badge-icons" aria-hidden="true"></i>{" "}
+            </div>
+          </NavLink>
+        </li>
+
+        <li className="list-item-inline">
+          <NavLink to="/playlist" activeClassName="nav-active-icon">
+            <div className="badge-div">
+              <i
+                class="fa fa-play-circle fa-lg badge-icons"
+                aria-hidden="true"
+              ></i>
+            </div>
+          </NavLink>
+        </li>
+        <li className="list-item-inline">
+          <NavLink to="/watchlater" activeClassName="nav-active-icon">
+            <div className="badge-div">
+              <i class="fa fa-clock-o fa-lg badge-icons" aria-hidden="true"></i>
+            </div>
+          </NavLink>
+        </li>
+        <li className="list-item-inline">
+          <NavLink to="/likedvideos" activeClassName="nav-active-icon">
+            <div className="badge-div">
+              <i
+                class="fa fa-thumbs-up fa-lg badge-icons"
+                aria-hidden="true"
+              ></i>
+            </div>
+          </NavLink>
+        </li>
+        {/* <li className="list-item-inline">
+          <NavLink to="/" activeClassName="nav-active-icon">
+            <div className="badge-div">
+              <i
+                class="fa fa-user-circle-o fa-lg badge-icons"
+                aria-hidden="true"
+              ></i>
+            </div>
+          </NavLink>
+        </li> */}
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
