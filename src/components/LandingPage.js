7100dcebed71a0d4382bc159959862c65de78cc1
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-04-30 06:02:24 +0530

    Adding usecontext

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,40 +2,43 @@ import React from "react";
 import SideBar from "./SideBar";
 import NavBar from "./NavBar";
 import { Link } from "react-router-dom";
-import videos from "../data/videos";
+import useVideos from "../context/videos-context";
 import useHistory from "../context/history-context";
 
 function LandingPage() {
   let { history, historydispatch } = useHistory();
+  let { videos } = useVideos();
+  console.log("videos", videos);
   return (
     <div className="landing-page-main">
       <NavBar />
       <SideBar />
       <div className="landing-page-div">
         <div className=" landing-page-vidoes videos">
-          {videos.map((item) => {
-            return (
-              <div
-                className="video"
-                onClick={(e) =>
-                  historydispatch({ type: "ADDTOHISTORY", payload: item.id })
-                }
-              >
-                <Link
-                  className="landing-page-videos"
-                  to={`/playvideo/${item.id}`}
+          {videos &&
+            videos.map((item) => {
+              return (
+                <div
+                  className="video"
+                  onClick={(e) =>
+                    historydispatch({ type: "ADDTOHISTORY", payload: item.id })
+                  }
                 >
-                  <img className="video-image" src={item.image} alt="NA" />
+                  <Link
+                    className="landing-page-videos"
+                    to={`/playvideo/${item.id}`}
+                  >
+                    <img className="video-image" src={item.image} alt="NA" />
 
-                  <div className="video-content">
-                    <div className="video-name">{item.name}</div>
-                    <div className="video-channel">{item.channel}</div>
-                    <div className="video-channel">31M views 2 years ago</div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
+                    <div className="video-content">
+                      <div className="video-name">{item.name}</div>
+                      <div className="video-channel">{item.channel}</div>
+                      <div className="video-channel">31M views 2 years ago</div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
