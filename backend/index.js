f3f66e1af27d4e31c1c6151f88a5431b334f7e53
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-05-16 15:48:05 +0530

    Updated port

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,7 @@ const playlistRouter = require("./routers/playlist.router");
 const initializeDBConnection = require("./db/db.connection");
 const allVideos = require("./data/videos");
 const Video = require("./models/video.model");
-
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
